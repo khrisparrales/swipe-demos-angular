f098a09a93b4a20fed947841058f6c872733294a
khrisparrales <lecristianot@gmail.com>
2020-08-28 18:34:43 -0500

    add card 4 files modified, 1 added

--- src/app/components/card-swipe/card-swipe.component.ts
+++ src/app/components/card-swipe/card-swipe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SwiperComponent, SwiperDirective, SwiperConfigInterface,
+  SwiperScrollbarInterface, SwiperPaginationInterface } from 'ngx-swiper-wrapper';
+
 @Component({
   selector: 'app-card-swipe',
   templateUrl: './card-swipe.component.html',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardSwipeComponent implements OnInit {
 
+ public config: SwiperConfigInterface = {  
+      spaceBetween: 30,
+     effect: 'fade',
+       loop: true,
+        mousewheel: {
+         invert: false,
+       },
+         pagination: {
+        el: '.blog-slider__pagination',
+         clickable: true,
+       }
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+// var swiper = new Swiper('.blog-slider', {
+//       spaceBetween: 30,
+//       effect: 'fade',
+//       loop: true,
+//       mousewheel: {
+//         invert: false,
+//       },
+//       // autoHeight: true,
+//       pagination: {
+//         el: '.blog-slider__pagination',
+//         clickable: true,
+//       }
+//     });
